303e21b0fc5dacf2dfe5c7254e545a0d244e1cac
Effat Chaar <effat.shaar@gmail.com>
2018-05-10 14:55:14 -0700

    the paddles are working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './styles/game.css';
 import Game from './partials/Game';
-import Board from './partials/Board';
 
 // create a game instance
 const game = new Game('game', 512, 256);
--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,4 +1,6 @@
-import { SVG_NS } from '../settings';
+import Paddle from './Paddle';
+import Board from './Board';
+import { SVG_NS, KEYS } from '../settings';
 
 export default class Game {
 
@@ -6,18 +8,42 @@ export default class Game {
 		this.element = element;
 		this.width = width;
 		this.height = height;
-
 		this.gameElement = document.getElementById(this.element);
-	
-		// Other code goes here...
-	}
 
+		this.board = new Board(this.width, this.height);
+		
+		this.paddleWidth = 8;
+		this.paddleHeight = 56;
+		this.boardGap = 10;
+
+		this.player1 = new Paddle(
+		this.height,
+		this.paddleWidth,
+		this.paddleHeight,
+		this.boardGap,
+		((this.height - this.paddleHeight) / 2),
+		KEYS.a,
+		KEYS.z
+		);
+		this.player2 = new Paddle(
+		this.height,
+		this.paddleWidth,
+		this.paddleHeight,
+		(this.width - this.boardGap - this.paddleWidth),
+		((this.height - this.paddleHeight) / 2),
+		KEYS.up,
+		KEYS.down
+		);
+	}
 	render() {
 			this.gameElement.innerHTML = '';
-			let svg = document.createElementNS(SVG_NS, "svg");
-			svg.setAttributeNS(null, "width", this.width);
-			svg.setAttributeNS(null, "height", this.height);
-			svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
+			let svg = document.createElementNS(SVG_NS, 'svg');
+			svg.setAttributeNS(null, 'width', this.width);
+			svg.setAttributeNS(null, 'height', this.height);
+			svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
 			this.gameElement.appendChild(svg);
+			this.board.render(svg);
+			this.player1.render(svg);
+			this.player2.render(svg);
 		}
 	}
